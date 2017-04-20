5472e3f1415739a896bf7b40903a5034af7ad7a1
Yinka Ash <netguru@Users-MacBook-Pro.local>
2017-04-20 12:15:00 +0800

    Edit Devise Config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,15 +85,15 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-config.action_mailer.default_url_options = { host: 'edu-global.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'getc-app.herokuapp.com' }
 
-ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-}
+  ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
+  }
 end
